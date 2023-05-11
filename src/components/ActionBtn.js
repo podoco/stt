4bc9c7239747160419bccafa8d073d24a779dd92
jikyubin <jikyubin@jikyubinui-MacBookPro.local>
2023-05-11 12:38:21 +0900

    fix: 파일 로컬 폴더 저장 1차

--- src/components/ActionBtn.js
+++ src/components/ActionBtn.js
@@ -1,25 +1,64 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
-import { useRecoilState } from "recoil";
-import { dataState } from "../store";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { dataState, dirHandleState } from "../store";
 
 export default function ActionBtn() {
-  const [data] = useRecoilState(dataState);
+  const data = useRecoilValue(dataState);
+  const [dirHandle, setDirHandle] = useRecoilState(dirHandleState);
+
+  async function saveFile(strData) {
+    if (dirHandle === null) {
+      alert("저장 폴더를 먼저 선택해주세요!");
+      return;
+    }
+    const fileHandle = await dirHandle.getFileHandle(`${data.fileName}.json`, {
+      create: true,
+    });
+    const writableStream = await fileHandle.createWritable({
+      keepExistingData: true,
+    });
+    await writableStream.write(strData);
+    await writableStream.close();
+  }
 
   const handleSaveClick = () => {
     const dataString = JSON.stringify(data, null, 4);
     const blob = new Blob([dataString], { type: "application/json" });
-    const url = URL.createObjectURL(blob);
-    const link = document.createElement("a");
-    link.download = `${data.fileName}.json`.replace(".json", "");
-    link.href = url;
-    link.click();
+    saveFile(blob);
   };
+
+  async function defineDirHandle() {
+    const dirHandle = await window.showDirectoryPicker({ mode: "readwrite" });
+    setDirHandle(dirHandle);
+  }
+
+  const handleSaveDirectionClick = () => {
+    defineDirHandle();
+  };
+
+  const preventClose = (e) => {
+    e.preventDefault();
+    e.returnValue = ""; //Chrome에서 동작하도록 함. deprecated될 예정이라고 함
+  };
+
+  useEffect(() => {
+    (() => {
+      window.addEventListener("beforeunload", preventClose);
+    })();
+
+    return () => {
+      window.removeEventListener("beforeunload", preventClose);
+    };
+  }, []);
 
   return (
     <Wrapper>
       <MergeBtn>병합하기</MergeBtn>
       <AddBtn>오른쪽에 추가</AddBtn>
+      <SaveDirectionBtn onClick={handleSaveDirectionClick}>
+        저장 폴더 선택
+      </SaveDirectionBtn>
       <SaveBtn onClick={handleSaveClick}>파일저장</SaveBtn>
     </Wrapper>
   );
@@ -60,6 +99,22 @@ const AddBtn = styled.button`
     background-color: #0174dfcc;
   }
 `;
+
+const SaveDirectionBtn = styled.button`
+  width: 150px;
+  height: 60px;
+  cursor: pointer;
+  font-size: 20px;
+  font-weight: bold;
+  background-color: #0174df;
+  border-radius: 15px;
+  color: white;
+
+  &:hover {
+    background-color: #0174dfcc;
+  }
+`;
+
 const SaveBtn = styled.button`
   width: 150px;
   height: 60px;
